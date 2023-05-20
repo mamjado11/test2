3608523e37365f22877611395e167fa940b1d03b
gnw <muhmmadamjadowies@gmail.com>
2023-05-20 14:35:20 +0300

    awesome dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_single_cascade_in_expression_statements
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, deprecated_member_use, avoid_print, use_key_in_widget_constructors, camel_case_types, avoid_single_cascade_in_expression_statements
 
-import 'package:awesome_dialog/awesome_dialog.dart';
-import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
+import 'package:awesome_dialog/awesome_dialog.dart';
 
 void main() {
   runApp(FirstApp());
@@ -25,34 +24,32 @@ class test extends StatefulWidget {
 }
 
 class _testState extends State<test> {
-  var text1 = "how are you ";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
-        drawer: Drawer(),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {
-                    AwesomeDialog(
-                      context: context,
-                      dialogType: DialogType.info,
-                      animType: AnimType.rightSlide,
-                      title: 'Dialog Title',
-                      desc: 'Dialog description here.............',
-                      btnCancelOnPress: () {},
-                      btnOkOnPress: () {},
-                    )..show();
-                  },
-                  child: Text("show alert "),
-                ),
-              )
-            ],
-          ),
-        ));
+      appBar: AppBar(),
+      drawer: Drawer(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                AwesomeDialog(
+                  context: context,
+                  dialogType: DialogType.info,
+                  animType: AnimType.rightSlide,
+                  title: 'Dialog Title',
+                  desc: 'Dialog description here.............',
+                  btnCancelOnPress: () {},
+                  btnOkOnPress: () {},
+                )..show();
+              },
+              child: Text("click me "),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
