4bda06e8df8f8831542556abf7c5bf6119fc3c78
gnw <muhmmadamjadowies@gmail.com>
2023-05-21 02:18:30 +0300

    solving dropdown search

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, deprecated_member_use, avoid_print, use_key_in_widget_constructors, camel_case_types, avoid_single_cascade_in_expression_statements
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sort_child_properties_last, deprecated_member_use, avoid_print, use_key_in_widget_constructors, camel_case_types, avoid_single_cascade_in_expression_statements, unused_import
 
 import 'package:flutter/material.dart';
 import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 
 void main() {
   runApp(FirstApp());
@@ -31,7 +32,7 @@ class _testState extends State<test> {
       drawer: Drawer(),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
             TextButton(
               onPressed: () {
@@ -46,6 +47,22 @@ class _testState extends State<test> {
                 )..show();
               },
               child: Text("click me "),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.all(10),
+              child: DropdownSearch<String>(
+                mode: Mode.MENU,
+                showSelectedItems: true,
+                items: ["Brazil", "Italia (Disabled)", "Tunisia", 'Canada'],
+                dropdownSearchDecoration: InputDecoration(
+                  labelText: "Menu mode",
+                  hintText: "country in menu mode",
+                ),
+                popupItemDisabled: (String s) => s.startsWith('I'),
+                onChanged: print,
+                selectedItem: "Brazil",
+              ),
             ),
           ],
         ),
